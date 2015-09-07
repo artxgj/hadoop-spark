67003fb56d4ffb42b5fc79c4ba9df91835eaba07
Art Kho <beijingbayou@gmail.com>
2015-09-07 07:44:32 -0700

    removed old scripts

--- cs246/recommend-friends/scripts/friend-recom-py-2.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-hadoop fs -rm -r /user/art/cs246/output/recommended-friends-2
-hdfs dfs -rm -r /user/art/tmp/friendrecom2
-$SPARK_HOME/bin/spark-submit --master yarn-cluster recommend-friends-2.py --py-files $SPARK_HOME/python/lib/pyspark.zip
