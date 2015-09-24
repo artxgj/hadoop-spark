c14713a6ea95cef1105e89e1adea4ee4ec913c10
Art Kho <beijingbayou@gmail.com>
2015-09-24 03:13:25 -0700

    added comments to apriori.py

--- cs246/association-rules/problem-description.md
+++ cs246/association-rules/problem-description.md
@@ -1,14 +1,14 @@
 
-## Stanford CS246: Mining Massive Data Sets Winter 2014 ##
+## [Stanford CS246: Mining Massive Data Sets Winter 2014](http://snap.stanford.edu/class/cs246-2014/handouts.html) ##
 
 [Problem Set 1, 2d and 2e](
-http://snap.stanford.edu/class/cs246-2014/handouts.html)
+http://web.stanford.edu/class/cs246/homeworks/hw1.pdf)
 
-Product Recommendations: The action or practice of selling additional products or services to existing customers is called cross-selling. Giving product recommendation is one of the examples of cross-selling that are frequently used by online retailers. One simple method to give product recommendations is to recommend products that are frequently browsed together by the customers.
+**Product Recommendations**: The action or practice of selling additional products or services to existing customers is called cross-selling. Giving product recommendation is one of the examples of cross-selling that are frequently used by online retailers. One simple method to give product recommendations is to recommend products that are frequently browsed together by the customers.
 Suppose we want to recommend new products to the customer based on the products they have already browsed on the online website. Write a program using the A-priori algorithm to find products which are frequently browsed together. Fix the support to s =100 (i.e. product pairs need to occur together at least 100 times to be considered frequent) and find itemsets of size 2 and 3.
 
 Use the online browsing behavior dataset at: http://snap.stanford.edu/class/cs246-data/ browsing.txt. Each line represents a browsing session of a customer. On each line, each string of 8 characters represents the id of an item browsed during that session. The items are separated by spaces.
 
-2d. List the top 5 rules with corresponding confidence scores in decreasing order of confidence score for itemsets of size 2. (Break ties, if any, by lexicographically increasing order on the left hand side of the rule.)
+**2d**. List the top 5 rules with corresponding confidence scores in decreasing order of confidence score for itemsets of size 2. (Break ties, if any, by lexicographically increasing order on the left hand side of the rule.)
 
-2e. List the top 5 rules with corresponding confidence scores in decreasing order of confidence score for itemsets of size 3. A rule is of the form: (item1, item2) ⇒ item3. (Order the left hand side pair lexicographically. Then break ties, if any, by lexicographical order of the first then the second item in the pair.)
+**2e**. List the top 5 rules with corresponding confidence scores in decreasing order of confidence score for itemsets of size 3. A rule is of the form: (item1, item2) ⇒ item3. (Order the left hand side pair lexicographically. Then break ties, if any, by lexicographical order of the first then the second item in the pair.)
--- cs246/association-rules/src/main/python/apriori.py
+++ cs246/association-rules/src/main/python/apriori.py
@@ -4,6 +4,11 @@ import getopt
 import sys
 import itertools
 
+"""
+A solution for Stanford Winter 2014 CS246's Product Recommendation problem. It
+generates correct recommendations for the browsing.txt dataset
+"""
+
 
 def parse_inputs(line):
     return frozenset() if line == '' else frozenset(line.split(' '))
@@ -13,49 +18,43 @@ def usage(exit_code=0):
     print "\nusage:\n"
     print "frequent-itemsets.py"
     print "  -i input_filepath"
-    print "  -o output_filepath"
     print "  -s support"
     sys.exit(exit_code)
 
 
-def confidence(i, j):
-    return j/i
-
-
-def pair_confidence(freq_i, freq_j):
-    t = []
-    for k, v in freq_j.items():
-        t.append(((k[0], k[1]), confidence(freq_i[k[0]], v)))
-        t.append(((k[1], k[0]), confidence(freq_i[k[1]], v)))
-
-    return t
-
-
-def triple_confidence(i, j):
-    t = []
-    n = len(j.iterkeys().next())
-    for k, v in j.items():
-        ikeys = itertools.combinations(k, n-1)
-        for ikey in ikeys:
-            if ikey in i:
-                t.append((tuple(list(ikey) + list(set(k) - set(ikey))), confidence(i[ikey], v)))
-
-    return t
-
-
 def prune_non_frequent(items_counts, support):
+    """
+    Create an frequent itemset
+    :param items_counts: a dictionary of items/counts
+    :param support: support threshold
+    :return: a dictionary representing the frequent itemset
+    """
     return {k: v for k, v in items_counts.iteritems() if v >= support}
 
 
-def candidate_k_tuples(items_set, k):
+def candidate_k_tuples(frequent_items_set, k):
+    """
+    Create candidate items sets of size k using frequent items-sets of size k-1
+    :param frequent_items_set: k-1 frequent items set
+    :param k: the size of candidate items sets.
+    :return:
+    """
     candidates = {}
-    for key_tuple in itertools.combinations(sorted(items_set), k):
+    for key_tuple in itertools.combinations(sorted(frequent_items_set), k):
         candidates[key_tuple] = 0
 
     return candidates
 
 
-def frequent_k_tuples(baskets, candidate_tuples, k, items_set):
+def count_candidate_k_tuples(baskets, candidate_tuples, k, items_set):
+    """
+    Generate frequencies of candidate items sets of size k
+    :param baskets: list of baskets; each basket is a set of items
+    :param candidate_tuples:
+    :param k: cardinality of candidate sets
+    :param items_set: frequent items for generating k-tuples of items
+    :return:
+    """
     for basket in baskets:
         basket_subset = sorted([item for item in basket if item in items_set])
         k_items = itertools.combinations(basket_subset, k)
@@ -65,30 +64,87 @@ def frequent_k_tuples(baskets, candidate_tuples, k, items_set):
 
 
 def find_frequent_pairs(baskets, frequent_singles, support):
+    """
+    Generate frequent associations of pairs of items { A->B }\
+
+    :param baskets: list of baskets; each basket is a set of items
+    :param frequent_singles: a dictionary representing frequent single items and their counts
+    :param support:
+    :return: a dictionary representing
+    """
     candidate_pairs = candidate_k_tuples(frequent_singles, 2)
-    frequent_k_tuples(baskets, candidate_pairs, 2, frequent_singles)
+    count_candidate_k_tuples(baskets, candidate_pairs, 2, frequent_singles)
     return prune_non_frequent(candidate_pairs, support)
 
 
 def find_frequent_triples(baskets, frequent_pairs, support):
-    triple_items_set = set([f for fp in frequent_pairs for f in fp])
-    candidate_triples = candidate_k_tuples(triple_items_set, 3)
-    frequent_k_tuples(baskets, candidate_triples, 3, triple_items_set)
+    """
+    Generate frequent associations of 3 items { A,B -> C }
+
+    :param baskets: list of baskets; each basket is a set of items
+    :param frequent_pairs: a dictionary representing frequent pairs of items and their counts
+    :param support: support threshold
+    :return:
+    """
+    items_set = set([f for fp in frequent_pairs for f in fp])
+    candidate_triples = candidate_k_tuples(items_set, 3)
+    count_candidate_k_tuples(baskets, candidate_triples, 3, items_set)
     return prune_non_frequent(candidate_triples, support)
 
 
+def confidence(i, j):
+    return j/i
+
+
+def pair_confidence(freq_i, freq_j):
+    """
+    conf(I->j) for pairs, i.e., Support(Union(I,j))/Support(I), where I and J have cardinality of 1
+    :param freq_i: a dictionary representing frequent single items and their counts
+    :param freq_j: a dictionary representing frequent pairs of items and their counts
+    :return:
+    """
+    t = []
+    for k, v in freq_j.items():
+        t.append(((k[0], k[1]), confidence(freq_i[k[0]], v)))  # conf(A->B)
+        t.append(((k[1], k[0]), confidence(freq_i[k[1]], v)))  # conf(B->A)
+
+    return t
+
+
+def triple_confidence(i, j):
+    """
+    conf(I->j) for itemsets of cardinality > 2, i.e., Support(Union(I,j))/Support(I), where I has cardinality of k and J
+    j has cardinality of 1
+
+    Note: tested only for triples, thus the function name triple_confidence; will have to test
+    for frequent items > 3
+
+    :param i: dictionary representing frequent items of size k-1 and their counts
+    :param j: dictionary representing frequent items of size k and their counts
+    :return:
+    """
+    t = []
+    n = len(j.iterkeys().next())
+    for k, v in j.items():
+        # for triples, generates Conf(A,B -> C), Conf(A,C->B) and Conf(B,C -> A) for triples if pairs are frequent
+        ikeys = itertools.combinations(k, n-1)
+        for ikey in ikeys:
+            if ikey in i:
+                t.append((tuple(list(ikey) + list(set(k) - set(ikey))), confidence(i[ikey], v)))
+
+    return t
+
+
 def main(args):
-    output_filepath = input_filepath = None
+    input_filepath = None
     support = 0
     try:
-        opts, args = getopt.getopt(args, "hi:o:s:")
+        opts, args = getopt.getopt(args, "hi:s:")
         for o, a in opts:
             if o == "-h":
                 usage()
             elif o == "-i":
                 input_filepath = a
-            elif o == "-o":
-                output_filepath = a
             elif o == "-s":
                 support = int(a)
             else:
@@ -98,7 +154,7 @@ def main(args):
     except ValueError:
         usage(2)
 
-    if input_filepath is None or output_filepath is None or support == 0:
+    if input_filepath is None or support == 0:
         usage(2)
 
     print "process singles"
@@ -117,22 +173,23 @@ def main(args):
             baskets.append(set(basket))
 
     frequent_singles = prune_non_frequent(singles, support)
-    print "process pairs"
+    print "Generate frequent pairs"
     frequent_pairs = find_frequent_pairs(baskets, frequent_singles, support)
-    print "process triples"
+    print "Generate frequent triples"
     frequent_triples = find_frequent_triples(baskets, frequent_pairs, support)
 
-    print "Top pairs by confidence"
+    print "Generate confidence of frequent itemsets of pairs"
     pc = pair_confidence(frequent_singles, frequent_pairs)
     pc.sort(key=lambda x: (-x[1], x[0]))
 
+    print "Top 15 pairs by confidence"
     for pair in pc[:15]:
         print "{} -> {} {}".format(pair[0][0], pair[0][1], pair[1])
 
-    print
-    print "Top triples by confidence"
+    print "Generate confidence of frequent itemsets of triples"
     tc = triple_confidence(frequent_pairs, frequent_triples)
     tc.sort(key=lambda x: (-x[1], x[0]))
+    print "Top 15 triples by confidence"
     for triple in tc[:15]:
         print "{}, {} -> {} {}".format(triple[0][0], triple[0][1], triple[0][2], triple[1])
 
