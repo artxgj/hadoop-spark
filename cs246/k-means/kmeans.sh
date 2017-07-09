9197ff4aff43f9bf236bf144cd00f7233afe109a
Art Kho <beijingbayou@gmail.com>
2017-07-09 05:39:22 -0700

    removed k-means

--- cs246/k-means/kmeans.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-hadoop fs -rm -r /user/art/cs246/k-means/output-centroids
-hadoop jar target/kmeans-1.0-SNAPSHOT.jar org.learningisfun.hadoop.Kmeans 20 /user/art/cs246/k-means/documents /user/art/cs246/k-means/random-centroids/c1.txt /user/art/cs246/k-means/output-centroids
